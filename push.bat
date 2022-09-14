997c74433f2c784d4c46ee4e108b9ee08bd08f49
cit142f-21@M307.UC-BCF.EDU.PH <cit142f-21@M307.UC-BCF.EDU.PH>
2022-09-14 16:49:46 +0800

    first upload

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
-git remote set-url origin https//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@github.com/rebaybmejett/dayum.git
+git remote set-url origin https//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@github.com/rebaybmejett/dayum.git
 git add .
 git commit -m "first upload"
 git push
