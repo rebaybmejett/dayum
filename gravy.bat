289e6d4c4b2afd0839fb5f452ee9409d7762891a
cit142f-21@M307.UC-BCF.EDU.PH <cit142f-21@M307.UC-BCF.EDU.PH>
2022-09-14 17:12:48 +0800

    first

---
+++ gravy.bat
@@ -0,0 +1,6 @@
+@echo off
+git remote set-url origin https://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@github.com/rebaybmejett/dayum.git
+git add .
+git commit -m "first"
+git push
+pause
--- push.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-git remote set-url origin https//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@github.com/rebaybmejett/dayum.git
-git add .
-git commit -m "first"
-git push
-pause
